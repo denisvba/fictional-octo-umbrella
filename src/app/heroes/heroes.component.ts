5b6dc49396b6b288ec016803951829206e111e53
Augusto Cesar <guto_slv@live.com>
2018-03-21 22:18:13 -0300

    Finishes Displaying a List

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -8,16 +8,18 @@ import { HEROES } from '../mock-heroes';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  heroes = HEROES;
   
-  hero: Hero = {
-    id: 1,
-    name: 'Windowstorm'
-  };
+  heroes = HEROES;
+
+  selectedHero: Hero;
 
   constructor() { }
 
   ngOnInit() {
   }
 
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 }
