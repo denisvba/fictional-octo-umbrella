1197b94d89b368f3a738e44807d4d5220434ec28
Augusto Cesar <guto_slv@live.com>
2018-03-28 18:04:57 -0300

    Fix typing error

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -13,7 +13,7 @@ export class HeroService {
   getHeroes(): Observable<Hero[]> {
     //Todo: send the message _after_ fetching  the heroes
     this.messageService.add('HeroService: fetched heroes');
-    return of(HEROES;
+    return of(HEROES);
   }
 
   getHero(id: number): Observable<Hero> {
